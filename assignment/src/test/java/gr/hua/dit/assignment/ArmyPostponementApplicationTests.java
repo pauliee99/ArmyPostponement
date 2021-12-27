cedb7811ec411476da8d89f82782f618772ec44d
Pavlos Andreou <pavlosan67@gmail.com>
2021-12-27 14:22:51 +0200

    commit fix branches

---
+++ assignment/src/test/java/gr/hua/dit/assignment/ArmyPostponementApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyPostponementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
